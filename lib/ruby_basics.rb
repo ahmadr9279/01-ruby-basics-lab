be3817b2a4fb6e0ceb8364131ae29b36f5a5bbdc
Ahmad Rabah <ahmadr9279@hstat.org>
2018-10-10 01:47:08 +0000

    complete review of methods,values, and variables

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,30 @@
-def division
+def division(num1,num2)
+    num1/num2
     
 end
 
-def argue
+def argue(argument)
+    argument
   
 end
 
-def greeting
+def greeting(argue1,argue2)
 
 end
 
 def return_a_value
+    return "Nice"
+    
     
 end
 
-def pizza_party
+def pizza_party(topping ="cheese")
+    return topping
     
 end
 
-def whisper
+def whisper(recieved_argument)
+    recieved_argument.downcase
+    
     
 end
